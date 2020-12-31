0b72b60a4b997b853bfa9b598ac2b32981c354f7
Md Iftekhar Tanveer <go2chayan@gmail.com>
2020-12-30 19:02:58 -0500

    Updated the example

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,2 +1,5 @@
 # Write all the epub meta in a new line delimited json file
 for afile in ~/*.epub; do python -m shobdokutir.ebook.parser --get_epub_meta $afile >> ~/epub_meta.json; done
+
+# Write all the epub contents in a new line delimited json file
+for afile in ~/*.epub; do python -m shobdokutir.ebook.parser --get_epub_text $afile >> ~/epub_contents.json; done
